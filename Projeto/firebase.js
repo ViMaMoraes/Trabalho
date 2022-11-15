4953990c800049833286cc132b48f7c1ff1adf70
ViMaMoraes <mvmoraes81@gmail.com>
2022-11-15 14:42:37 -0300

    Login

---
+++ Projeto/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+const firebaseConfig = {
+    apiKey: "AIzaSyCma9cPjREXst6vnZmKB9fjK40caZ_fKcw",
+    authDomain: "projeto-final-2a985.firebaseapp.com",
+    projectId: "projeto-final-2a985",
+    storageBucket: "projeto-final-2a985.appspot.com",
+    messagingSenderId: "888497864608",
+    appId: "1:888497864608:web:873e2b71c7c25a45152ec5",
+    measurementId: "G-L8D5G7BBHD"
+};
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig)
+var database = firebase.database()
